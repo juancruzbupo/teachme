9adcee3c1db54272535b026873b8fecb6503aedd
juancruzbupo <juancruzbupo@gmail.com>
2016-05-23 08:34:23 -0300

    agrega componente html

---
+++ app/Providers/HtmlServiceProvider.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace TeachMe\Providers;
+
+use Collective\Html\HtmlServiceProvider as CollectiveHtmlServiceProvider;
+use TeachMe\Components\HtmlBuilder;
+
+/**
+ * summary
+ */
+class HtmlServiceProvider extends CollectiveHtmlServiceProvider
+{
+    /**
+     * Register the HTML builder instance.
+     *
+     * @return void
+     */
+    protected function registerHtmlBuilder()
+    {
+        $this->app->singleton('html', function ($app) {
+            return new HtmlBuilder($app['url'], $app['view']);
+        });
+    }
+
+}
--- config/app.php
+++ config/app.php
@@ -156,6 +156,8 @@ return [
         TeachMe\Providers\EventServiceProvider::class,
         TeachMe\Providers\RouteServiceProvider::class,
 
+        TeachMe\Providers\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -201,6 +203,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
